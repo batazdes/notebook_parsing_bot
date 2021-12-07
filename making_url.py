b83cf356c2d324d2042bdc69b28e88f3882b6348
batazdes <91262020+batazdes@users.noreply.github.com>
2021-12-07 12:39:25 +0300

    Update making_url.py

--- making_url.py
+++ making_url.py
@@ -27,13 +27,155 @@ def changing_url():
         url=url+'%2C2589_3&price_min='+price_min+'&price_max='+price_max
 
         manu = making_url(parament='chosen_manufacture',url=url,added='%2C',right_board='%2C2589_3&pf=discount.any%2Crating.any',left_board='f=discount.any%2Crating.any',replaced_pass='',what_replaced='?',data=data)
-        proc = making_url(parament='chosen_processor',url=manu[1],added='%2C277_3',right_board='%2C2589_3&pf=discount.any%2Crating.any',left_board=manu[0],replaced_pass='d1',what_replaced=' ',data=data)
-        resol = making_url(parament='chosen_resolution',url=proc[1],added='%2C2788_3',right_board=proc[0],left_board=manu[0],replaced_pass='kh',what_replaced='х',data=data)
-        matr = making_url(parament='chosen_matrix',url=resol[1],added='%2C14630_3',right_board=resol[0],left_board=manu[0],replaced_pass='',what_replaced='?',data=data)
-        ssd = making_url(parament='chosen_ssd_size',url = matr[1],added='%2C18332_3',right_board='%2C2589_3&pf=discount.any%2Crating.any',left_board=proc[0],replaced_pass='d1',what_replaced=' ',data=data)
+        if manu[0]=='h':
+            left_board = 'f=discount.any%2Crating.any'
+            url = url
+            proc = making_url(parament='chosen_processor', url=url, added='%2C277_3',
+                              right_board='%2C2589_3&pf=discount.any%2Crating.any', left_board=left_board,
+                              replaced_pass='d1', what_replaced=' ', data=data)
+        else:
+            proc = making_url(parament='chosen_processor', url=manu[1], added='%2C277_3',
+                              right_board='%2C2589_3&pf=discount.any%2Crating.any', left_board=manu[0],
+                              replaced_pass='d1', what_replaced=' ', data=data)
+
+        if proc[0]=='h':
+            if manu[0]=='h':
+                left_board = 'f=discount.any%2Crating.any'
+                right_board = '%2C2589_3&pf=discount.any%2Crating.any'
+                url = url
+                resol = making_url(parament='chosen_resolution',url=url,added='%2C2788_3',right_board=right_board,
+                                   left_board=left_board,replaced_pass='kh',what_replaced='х',data=data)
+            else:
+                right_board = '%2C2589_3&pf=discount.any%2Crating.any'
+                url=manu[1]
+                resol = making_url(parament='chosen_resolution', url=url, added='%2C2788_3', right_board=right_board,
+                                   left_board=manu[0], replaced_pass='kh', what_replaced='х', data=data)
+        else:
+            if manu[0]=='h':
+                left_board = 'f=discount.any%2Crating.any'
+                url = proc[1]
+                resol = making_url(parament='chosen_resolution', url=url, added='%2C2788_3', right_board=proc[0],
+                                   left_board=left_board, replaced_pass='kh', what_replaced='х', data=data)
+            else:
+                resol = making_url(parament='chosen_resolution', url=proc[1], added='%2C2788_3', right_board=proc[0],
+                                   left_board=manu[0], replaced_pass='kh', what_replaced='х', data=data)
+
+        if resol[0]=='h':
+            if proc[0]=='h':
+                if manu[0]=='h':
+                    left_board = 'f=discount.any%2Crating.any'
+                    right_board = '%2C2589_3&pf=discount.any%2Crating.any'
+                    url = url
+                    matr = making_url(parament='chosen_matrix', url=url, added='%2C14630_3',
+                                       right_board=right_board,
+                                       left_board=left_board, replaced_pass='', what_replaced='?', data=data)
+                else:
+                    right_board = '%2C2589_3&pf=discount.any%2Crating.any'
+                    left_board =manu[0]
+                    url = manu[1]
+                    matr = making_url(parament='chosen_matrix', url=url, added='%2C14630_3',
+                                      right_board=right_board,
+                                      left_board=left_board, replaced_pass='', what_replaced='?', data=data)
+            if proc[0]!='h':
+                if manu[0]=='h':
+                    left_board = 'f=discount.any%2Crating.any'
+                    right_board = proc[0]
+                    url = proc[1]
+                    matr = making_url(parament='chosen_matrix', url=url, added='%2C14630_3',
+                                      right_board=right_board,
+                                      left_board=left_board, replaced_pass='', what_replaced='?', data=data)
+                if manu[0]!='h':
+                    left_board = manu[0]
+                    right_board = proc[0]
+                    url = proc[1]
+                    matr = making_url(parament='chosen_matrix', url=url, added='%2C14630_3',
+                                      right_board=right_board,
+                                      left_board=left_board, replaced_pass='', what_replaced='?', data=data)
+        else:
+            if proc[0]=='h':
+                if manu[0]=='h':
+                    url = resol[1]
+                    right_board = resol[0]
+                    left_board = 'f=discount.any%2Crating.any'
+                    matr = making_url(parament='chosen_matrix', url=url, added='%2C14630_3',
+                                      right_board=right_board,
+                                      left_board=left_board, replaced_pass='', what_replaced='?', data=data)
+                else:
+                    matr = making_url(parament='chosen_matrix', url=resol[1], added='%2C14630_3', right_board=resol[0],
+                                      left_board=manu[0], replaced_pass='', what_replaced='?', data=data)
+            else:
+                if manu[0]=='h':
+                    url = resol[1]
+                    right_board = resol[0]
+                    left_board = 'f=discount.any%2Crating.any'
+                    matr = making_url(parament='chosen_matrix', url=url, added='%2C14630_3',
+                                      right_board=right_board,
+                                      left_board=left_board, replaced_pass='', what_replaced='?', data=data)
+                else:
+                    matr = making_url(parament='chosen_matrix', url=resol[1], added='%2C14630_3', right_board=resol[0],
+                                      left_board=manu[0], replaced_pass='', what_replaced='?', data=data)
+
+        if proc[0]=='h':
+            if resol[0]=='h':
+                if matr[0]=='h':
+                    if manu[0]=='h':
+                        left_board = 'f=discount.any%2Crating.any'
+                        ssd = making_url(parament='chosen_ssd_size', url=url, added='%2C18332_3',
+                                         right_board='%2C2589_3&pf=discount.any%2Crating.any', left_board=left_board,
+                                         replaced_pass='d1', what_replaced=' ', data=data)
+                        print(ssd)
+                    else:
+                        left_board = manu[0]
+                        ssd = making_url(parament='chosen_ssd_size', url=manu[1], added='%2C18332_3',
+                                         right_board='%2C2589_3&pf=discount.any%2Crating.any', left_board=left_board,
+                                         replaced_pass='d1', what_replaced=' ', data=data)
+                else:
+                    left_board = matr[0]
+                    ssd = making_url(parament='chosen_ssd_size', url=matr[1], added='%2C18332_3',
+                                     right_board='%2C2589_3&pf=discount.any%2Crating.any', left_board=left_board,
+                                     replaced_pass='d1', what_replaced=' ', data=data)
+            else:
+                left_board = resol[0]
+                if matr[0]=='h':
+                    ssd = making_url(parament='chosen_ssd_size', url=resol[1], added='%2C18332_3',
+                                     right_board='%2C2589_3&pf=discount.any%2Crating.any', left_board=left_board,
+                                     replaced_pass='d1', what_replaced=' ', data=data)
+                else:
+                    ssd = making_url(parament='chosen_ssd_size', url=matr[1], added='%2C18332_3',
+                                     right_board='%2C2589_3&pf=discount.any%2Crating.any', left_board=left_board,
+                                     replaced_pass='d1', what_replaced=' ', data=data)
+        else:
+            left_board = proc[0]
+            if matr[0]=='h':
+                if resol[0]=='h':
+                    ssd = making_url(parament='chosen_ssd_size', url=proc[1], added='%2C18332_3',
+                                     right_board='%2C2589_3&pf=discount.any%2Crating.any', left_board=left_board,
+                                     replaced_pass='d1', what_replaced=' ', data=data)
+                else:
+                    ssd = making_url(parament='chosen_ssd_size', url=resol[1], added='%2C18332_3',
+                                     right_board='%2C2589_3&pf=discount.any%2Crating.any', left_board=left_board,
+                                     replaced_pass='d1', what_replaced=' ', data=data)
+            else:
+                ssd = making_url(parament='chosen_ssd_size', url=matr[1], added='%2C18332_3',
+                                 right_board='%2C2589_3&pf=discount.any%2Crating.any', left_board=left_board,
+                                 replaced_pass='d1', what_replaced=' ', data=data)
+
         RAM = data.get('chosen_RAM_size').split(',')
         if RAM == ['']:
-            url_final = ssd[1]
+            if ssd[0]!='h':
+                url_final = ssd[1]
+            else:
+                if matr[0] != 'h':
+                    url_final = matr[1]
+                else:
+                    if resol[0] != 'h':
+                        url_final = resol[1]
+                    else:
+                        if proc[0] != 'h':
+                            url_final = proc[1]
+                        else:
+                            if manu[0] != 'h':
+                                url_final = manu[1]
         else:
             RAM.pop(-1)
             param = sorted(list(set(RAM)))
@@ -48,10 +190,30 @@ def changing_url():
                 if item == '32 gb':
                     item = '%2C4812_3'
                 text = text + item
-            url = ssd[1].split('%2C2589_3&pf=discount.any%2Crating.any')
-            url = [y for x in url for y in x.split(ssd[0])]
-            url_final = url[0] + ssd[0] + text + '%2C2589_3&pf=discount.any%2Crating.any' + url[-1]
-        with open('url.txt','w') as file:
+                if ssd[0] != 'h':
+                    link = ssd[1]
+                    variable = ssd[0]
+                else:
+                    if matr[0] != 'h':
+                        link = matr[1]
+                        variable = matr[0]
+                    else:
+                        if resol[0] != 'h':
+                            link = resol[1]
+                            variable = resol[0]
+                        else:
+                            if proc[0] != 'h':
+                                link = proc[1]
+                                variable = proc[0]
+                            else:
+                                if manu[0] != 'h':
+                                    link = manu[1]
+                                    variable = manu[0]
+                url = link.split('%2C2589_3&pf=discount.any%2Crating.any')
+                if url == link:
+                    url = link.split('%2C2589_3&price')
+                url_final = url[0] + text + '%2C2589_3&price' + url[-1]
+        with open('url.txt', 'w') as file:
             file.write(url_final)
 
 def main():
